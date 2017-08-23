9376c19091bfac9b1eb6fd0a76a233688e693d14
Mario Jorge <mariojorge.sdei@gmail.com>
2017-08-23 17:32:12 -0300

    Subida do projeto para branch

---
+++ src/br/com/encontrarfacil/util/FabricaDeConexoesSingleton.java
@@ -0,0 +1,26 @@
+package br.com.encontrarfacil.util;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class FabricaDeConexoesSingleton {
+
+	private static EntityManager em;
+	
+	public FabricaDeConexoesSingleton() {
+	}
+	
+	public static EntityManager getInstance() {
+		if(em == null) {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("EncontrarFacil");
+			
+			em =  emf.createEntityManager();
+		}
+		
+		return em;
+	}
+	
+	
+}
